4164aec6c1799c8553d2782a38fd4f2890a38928
Nanda <138947445+Nandaxy@users.noreply.github.com>
2023-11-14 08:57:39 +0700

    Update route.js

--- src/app/api/(menu)/test/route.js
+++ src/app/api/(menu)/test/route.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
 
-async function getRandomResponse(request) {
+async function GET(request) {
     try {
         const jsonData = fs.readFileSync('data/test.json', 'utf-8');
         const urls = JSON.parse(jsonData);
 
-        // Pilih URL secara acak
+        // Mendapatkan URL secara acak
         const randomIndex = Math.floor(Math.random() * urls.length);
         const randomUrl = urls[randomIndex];
 
-        // Kirim respons dengan URL yang dipilih dan status code 200 (OK)
-        return new Response(JSON.stringify({ selectedUrl: randomUrl }), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' }
-        });
+        // Menambahkan informasi status code dan nama penulis
+        const responseBody = {
+            status: 'success',
+            data: {
+                url: randomUrl,
+                author: 'Nanda'
+            }
+        };
+
+        // Mengembalikan respons dengan informasi status code, nama penulis, dan URL acak
+        return new Response(JSON.stringify(responseBody), { headers: { 'Content-Type': 'application/json' }, status: 200 });
     } catch (error) {
         console.error(error);
-        // Jika terjadi kesalahan, kirim respons dengan status code 500 (Internal Server Error)
         return new Response('Internal Server Error', { status: 500 });
     }
 }
 
-module.exports = { getRandomResponse };
+module.exports = { GET };
