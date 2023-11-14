ff5ad7752ca0b8be0a990d9fe7768de58ea7f795
Nanda <138947445+Nandaxy@users.noreply.github.com>
2023-11-14 09:04:15 +0700

    Update route.js

--- src/app/api/(menu)/test/route.js
+++ src/app/api/(menu)/test/route.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 
+// Inisialisasi variabel global untuk menyimpan indeks terakhir
+let lastUsedIndex = -1;
+
 async function GET(request) {
     try {
         const jsonData = fs.readFileSync('data/test.json', 'utf-8');
         const urls = JSON.parse(jsonData);
 
-        // Mendapatkan URL secara acak
-        const randomIndex = Math.floor(Math.random() * urls.length);
+        // Memastikan indeks yang dihasilkan berbeda dari yang terakhir kali digunakan
+        let randomIndex;
+        do {
+            randomIndex = Math.floor(Math.random() * urls.length);
+        } while (randomIndex === lastUsedIndex);
+
+        // Menyimpan indeks terakhir yang digunakan
+        lastUsedIndex = randomIndex;
+
+        // Mendapatkan URL menggunakan indeks yang dihasilkan
         const randomUrl = urls[randomIndex];
 
         // Menambahkan informasi status code dan nama penulis
