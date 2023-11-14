b04ae49e8c4cab0f297333602fc9cd1c5e6999b6
Nanda <138947445+Nandaxy@users.noreply.github.com>
2023-11-14 08:54:34 +0700

    Update route.js

--- src/app/api/(menu)/test/route.js
+++ src/app/api/(menu)/test/route.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 
-async function GET(request) {
+async function getRandomResponse(request) {
     try {
         const jsonData = fs.readFileSync('data/test.json', 'utf-8');
-        
-        return new Response(jsonData, { headers: { 'Content-Type': 'application/json' } });
+        const urls = JSON.parse(jsonData);
+
+        // Pilih URL secara acak
+        const randomIndex = Math.floor(Math.random() * urls.length);
+        const randomUrl = urls[randomIndex];
+
+        // Kirim respons dengan URL yang dipilih dan status code 200 (OK)
+        return new Response(JSON.stringify({ selectedUrl: randomUrl }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+        });
     } catch (error) {
         console.error(error);
+        // Jika terjadi kesalahan, kirim respons dengan status code 500 (Internal Server Error)
         return new Response('Internal Server Error', { status: 500 });
     }
 }
 
-module.exports = { GET };
+module.exports = { getRandomResponse };
